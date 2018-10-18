28f78a006fc51c138938a0a38bd24fa787871142
notsilverman <birchba@gmail.com>
2018-10-18 14:11:58 -0500

    fixed a picture

--- client/src/components/Question3/Question3.js
+++ client/src/components/Question3/Question3.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./Question3.css";
 import chili1 from "../../images/chili1.jpg";
 import chili2 from "../../images/chili5.JPG";
-import chili3 from "../../images/chili4.jpg";
+import chili3 from "../../images/chili6.jpeg";
 
 const Question3 = (props) => {
 
